5561de95d6e3a491a14db4ca0b912b18dff4ce38
Gyurcsó-Licsman Péter <h357395@stud.u-szeged.hu>
2025-10-17 11:06:22 +0200

    feat(#11): Tájak oldal

--- src/app/pages/world/lands/lands.component.ts
+++ src/app/pages/world/lands/lands.component.ts
@@ -8,12 +8,6 @@ import { setBackground } from '../../../shared/functional/functions';
   styleUrl: './lands.component.scss',
 })
 export class LandsComponent {
-  lands: { name: string; desc: string; img: string }[] = [
-    { name: 'Folyóköz', desc: '', img: 'folyokoz.jpg' },
-    { name: 'Nyugati Toronyvárosok', desc: '', img: 'nyugatitorony.png' },
-    { name: 'Kelet Népe', desc: '', img: 'keletnepe.png' },
-  ];
-
   ngOnInit() {
     setBackground('paper_bg.jpg');
   }
